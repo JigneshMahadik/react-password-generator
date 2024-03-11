9f685c50de520629f0aa2a6198e13906f36eaaa7
JigneshMahadik <149412247+JigneshMahadik@users.noreply.github.com>
2024-03-12 00:35:26 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PwdGen from './components/password-generator';
+
+function App() {
+  return (
+    <div className="App">
+      <PwdGen/>
+    </div>
+  );
+}
+
+export default App;
